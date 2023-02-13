12a18eb57b9f8cc6471d0638b67218a0a161ee76
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2023-02-13 03:24:37 +0100

    Add war export configuration

--- src/main/java/io/integratedproject/spring_car_rental/SpringCarRentalApplication.java
+++ src/main/java/io/integratedproject/spring_car_rental/SpringCarRentalApplication.java
@@ -2,13 +2,14 @@ package io.integratedproject.spring_car_rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
-public class SpringCarRentalApplication {
+public class SpringCarRentalApplication extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
         SpringApplication.run(SpringCarRentalApplication.class, args);
